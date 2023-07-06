7c0e436d43895e018c54ac09ff0e1c5960a6a9b3
nano13 <nano13@gmx.net>
2023-07-06 23:21:39 +0200

    implemented AimTarget

--- Player/AimTarget.gd
+++ Player/AimTarget.gd
@@ -10,21 +10,35 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# place AimTarget at the collision point facing towards the normal of the given surface
 	if ray.is_colliding():
 		var collision_point := ray.get_collision_point()
 		var collision_normal := ray.get_collision_normal()
 		global_transform.origin = collision_point + collision_normal * 0.01 # 0.01 is the distance of the sprite above the target
 		
-		# https://godotforums.org/d/30764-lookat-fails-sometimes
-		var up: Vector3 = global_transform.basis.y.normalized()
-		if (abs(collision_normal.y) > 0.99):
-			up = Vector3(0, 0, 1)
-		if (abs(collision_normal.z) > 0.99):
-			up = Vector3(0, 1, 0)
-		
-		look_at(collision_point - collision_normal, up)
+		look_at_safe(collision_point, collision_normal)
 		
 		show()
 	
 	else:
 		hide()
+
+func look_at_safe(point: Vector3, normal: Vector3) -> void:
+	# https://godotforums.org/d/30764-lookat-fails-sometimes
+	# we need to check for a valid up-vector to avoid gimbal locks
+	var up: Vector3 = global_transform.basis.y.normalized()
+	#if (abs(collision_normal.x) > 0.99):
+	#	print("xxxx")
+	if (abs(normal.y) > 0.99):
+		up = Vector3.RIGHT
+	if (abs(normal.z) > 0.99):
+		up = Vector3.UP
+	
+	var normal_abs: Vector3 = abs(normal)
+	var normal_up: Vector3 = abs(up)
+	if (round(normal_abs.x * 1000) == round(normal_up.x * 1000) and
+	 round(normal_abs.y * 1000) == round(normal_up.y * 1000) and
+	 round(normal_abs.z * 1000) == round(normal_up.z * 1000)):
+		up = Vector3.UP
+	
+	look_at(point - normal, up)
