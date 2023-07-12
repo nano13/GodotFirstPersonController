c4e98fc32a047e8250d3e4ed9e09dbb676b4d042
nano13 <nano13@gmx.net>
2023-07-12 14:38:10 +0200

    rotate motorbike

--- Scenes/Camera3DMotorbike.gd
+++ Scenes/Camera3DMotorbike.gd
@@ -4,21 +4,31 @@ extends Camera3D
 
 var look_dir: Vector2 # Input direction for look/aim
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		look_dir = event.relative * 0.0001
 		_rotate_camera()
 
+func _input(event: InputEvent) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	_handle_joypad_camera_rotation(delta)
+
+func _process(delta: float):
+	pass
+
 func _rotate_camera(sens_mod: float = 1.0) -> void:
 	rotation.y -= look_dir.x * camera_sens * sens_mod
 	rotation.x = clamp(rotation.x - look_dir.y * camera_sens * sens_mod, -1.5, 1.5)
+
+func _handle_joypad_camera_rotation(delta: float, sens_mod: float = 1.0) -> void:
+	var joypad_dir: Vector2 = Input.get_vector("look_left","look_right","look_up","look_down")
+	if joypad_dir.length() > 0:
+		look_dir += joypad_dir * delta
+		_rotate_camera(sens_mod)
+		look_dir = Vector2.ZERO
