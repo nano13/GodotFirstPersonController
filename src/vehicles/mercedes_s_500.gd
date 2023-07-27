80a1fa5b7a178a260ecd5f72a91d3a54dfb8c847
nano13 <nano13@gmx.net>
2023-07-27 14:20:48 +0200

    detaching wheels from car model and adding them to the VehicleWheel3D

--- src/vehicles/mercedes_s_500.gd
+++ src/vehicles/mercedes_s_500.gd
@@ -2,6 +2,8 @@ class_name VehicleBody3DMercedesS500 extends VehicleBody3D
 
 # https://www.b3dassets.com/2021/05/29/cars-3d-model-library/t
 
+@export_range(0, 100, 1) var brake_force: float = 100
+
 var max_rpm = 600
 var torque_min = 5000
 
@@ -34,29 +36,31 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("use"):
-		if camera_car.current:
-			exit_car()
-		elif is_player_in_range:
+	if is_player_in_range:
+		if Input.is_action_just_pressed("use"):
 			enter_car()
+	elif camera_car.current:
+		if Input.is_action_just_pressed("use"):
+			exit_car()
 
 func _physics_process(delta):
-	if camera_car.current:
-		print(basis.get_rotation_quaternion())
-	
 	freeze_if_not_used_and_crashed()
 	
-	# for some reason Input.get_axis will not work properly if called from _integrate_forces
-	# but we need to use it there, so we put it into a variable here
-	
-	var axis_left_right: float = Input.get_axis("vehicle_right", "vehicle_left")
-	
-	var speed: float = linear_velocity.length()
-	# to make it more realistic, the faster we go, the less we can turn the wheel
-	var steerdamp: float = clamp(speed/1.5, 2, 50)
-	var turn: float = axis_left_right / steerdamp
-	# smooth out our steering movements
-	steering = lerp(steering, turn, .1)
+	#print(angular_velocity.length())
+	if camera_car.current:
+		print(basis.get_rotation_quaternion())
+		
+		# for some reason Input.get_axis will not work properly if called from _integrate_forces
+		# but we need to use it there, so we put it into a variable here
+		
+		var axis_left_right: float = Input.get_axis("vehicle_right", "vehicle_left")
+		
+		var speed: float = linear_velocity.length()
+		# to make it more realistic, the faster we go, the less we can turn the wheel
+		var steerdamp: float = clamp(speed/1.5, 2, 50)
+		var turn: float = axis_left_right / steerdamp
+		# smooth out our steering movements
+		steering = lerp(steering, turn, .1)
 	
 	calculate_acceleration(delta)
 
@@ -64,26 +68,28 @@ func calculate_acceleration(delta: float):
 	if camera_car.current:
 		var rpm: float = clamp(wheel_front_right.get_rpm(), -1 * max_rpm, 0)
 		
-		#steering = lerp(steering, Input.get_axis("vehicle_right", "vehicle_left") * 0.4, 5 * delta)
-		#####steering = lerp(steering, Input.get_axis("move_right", "move_left") * 0.4, 5 * delta)
-		
 		var acceleration: float = Input.get_axis("vehicle_accelerate", "vehicle_decelerate")
 		var force: float = acceleration * torque_min * ( 1 - rpm / max_rpm)
 		wheel_front_right.engine_force = force
 		wheel_front_left.engine_force = force
 		
 		if Input.is_action_pressed("jump_default"):
-			set_brake(200)
+			set_brake(brake_force)
 		if Input.is_action_just_released("jump_default"):
 			set_brake(0)
+	else:
+		# make sure the car does not keep going when the player exits while pressing "vehicle_forward"
+		wheel_front_right.engine_force = 0
+		wheel_front_left.engine_force = 0
 
 func freeze_if_not_used_and_crashed():
-	if (wheel_front_left.is_in_contact() and wheel_front_right.is_in_contact() and
-	 wheel_rear_left.is_in_contact() and wheel_rear_right.is_in_contact() and
-	 not camera_car.current):
-		set_freeze_enabled(true)
-	elif camera_car.current:
-		set_freeze_enabled(false)
+	if linear_velocity.length() < 0.1:
+		if (wheel_front_left.is_in_contact() and wheel_front_right.is_in_contact() and
+		 wheel_rear_left.is_in_contact() and wheel_rear_right.is_in_contact() and
+		 not camera_car.current):
+			set_freeze_enabled(true)
+		elif camera_car.current:
+			set_freeze_enabled(false)
 
 func _on_area_3d_body_entered(body):
 	if body.name == "Player":
@@ -96,6 +102,8 @@ func _on_area_3d_body_exited(body):
 		is_player_in_range = false
 
 func enter_car():
+	set_brake(0)
+	
 	if not is_crashed:
 		camera_car.current = true
 		player.on_deactivate()
@@ -106,5 +114,7 @@ func enter_car():
 		ray.set_clicks_enabled(false)
 
 func exit_car():
+	set_brake(brake_force)
+	
 	camera_car.current = false
 	player.on_activate()
